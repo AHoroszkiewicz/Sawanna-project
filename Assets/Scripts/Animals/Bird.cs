88d9537785679756ac0b7bec1d5789b3feb49fa6
JGornas <jjgornas@gmail.com>
2023-12-13 19:13:07 +0100

    Revert "Revert "minor updates""
    This reverts commit 53b6ced7d970bb97c3185a793ddbde89fdea9d7e.

--- Assets/Scripts/Animals/Bird.cs
+++ Assets/Scripts/Animals/Bird.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Bird : Animal
 {
+    [SerializeField] private int Altitude { get; set; }
+    [SerializeField] private bool isHungry { get; set; }
+    [SerializeField] private int HungerLevel { get; set; }
+
     override public void Move()
     {
         Debug.Log("The birb flies");
