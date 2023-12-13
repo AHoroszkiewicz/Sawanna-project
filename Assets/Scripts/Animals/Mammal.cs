27c191cb762cb0772c10b6bfc6ab5c653a6cbfcc
JGornas <jjgornas@gmail.com>
2023-12-13 18:51:33 +0100

    minor updates
    Fields in birds mammals and snakes updated

--- Assets/Scripts/Animals/Mammal.cs
+++ Assets/Scripts/Animals/Mammal.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public abstract class Mammal : Animal
 {
+    [SerializeField] private int ID { get; set; }
+    [SerializeField] private bool IsHungry { get; set; }
+    [SerializeField] private int HungerLevel { get; set; }
+
     override public void Move()
     {
         Debug.Log("The Mammal moves");
--- Assets/Scripts/Animals/Snake.cs
+++ Assets/Scripts/Animals/Snake.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Snake : Animal
 {
+    [SerializeField] private int VenomLevel { get; set; }
+    [SerializeField] private int VenomRegenRate { get; set; }
+
     override public void Move()
     {
         Debug.Log("The snake slithers");
