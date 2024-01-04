a6d3a8bb05532521a15b14ec9aa34786eb76c28c
Maksymilian <mieczkowskimaksymilian001@gmail.com>
2024-01-04 19:38:51 +0100

    All nodes connected + edited node.cs

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -6,10 +6,16 @@ public class Node : MonoBehaviour
 {
     [SerializeField] private List<Node> connectedNodes = new List<Node>();
     [SerializeField] private NodeType nodeType = NodeType.none;
+    [SerializeField] private bool isOccupied = false; 
 
     public List<Node> ConnectedNodes
     {
         get { return connectedNodes; }
+    }
+
+    public bool IsOccupied 
+    {
+        get { return isOccupied; }
     }
 
     public enum NodeType
@@ -20,5 +26,21 @@ public class Node : MonoBehaviour
         bird = 3,
         hyena = 4,
         snake = 5,
+    }
+
+    // Sprawdza, czy node jest wolny
+    public bool IsNodeFree()
+    {
+        bool isFree = !isOccupied;
+        Debug.Log($"Node is free: {isFree}");
+        return isFree;
+    }
+
+    // Sprawdza, czy konkretne zwierze moze wejsc na ten node
+    public bool CanAnimalEnter(NodeType animal)
+    {
+        bool canEnter = !isOccupied;
+        Debug.Log($"Can animal enter: {canEnter}");
+        return canEnter;
     }
 }
